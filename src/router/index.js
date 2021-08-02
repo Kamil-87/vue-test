0f2face68121934576aebd9ebd062af47bc59778
kamil-87 <k.kamil87-87@yandex.ru>
2021-08-02 21:27:49 +0500

    refactoring: add mode history or hash

--- src/router/index.js
+++ src/router/index.js
@@ -3,34 +3,42 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      meta: {layout: 'main'},
-      component: () => import("@/views/Home"),
-    },
-    {
-      path: '/post/:id',
-      name: 'post',
-      meta: {layout: 'main'},
-      component: () => import("@/views/Post"),
-    },
-    {
-      path: "/auth",
-      name: "auth",
-      meta: {layout: 'empty'},
-      component: () => import("@/views/Login")
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      meta: {layout: 'main'},
-      component: () => import("@/views/Profile")
-    },
-  ]
-});
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    meta: {layout: 'main'},
+    component: () => import("@/views/Home"),
+  },
+  {
+    path: '/post/:id',
+    name: 'post',
+    meta: {layout: 'main'},
+    component: () => import("@/views/Post"),
+  },
+  {
+    path: "/auth",
+    name: "auth",
+    meta: {layout: 'empty'},
+    component: () => import("@/views/Login")
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: {layout: 'main'},
+    component: () => import("@/views/Profile")
+  },
+]
 
+let mode = 'history'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'hash'
+}
+
+const router = new VueRouter({
+  mode: mode,
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
